14e9559140d95ec3f765cc379f13f5d55a7402ba
Milanstha16 <sthamilan0000@gmail.com>
2026-02-16 14:26:50 +0545

    DB

--- db/db.js
+++ db/db.js
@@ -7,6 +7,8 @@ const DB_URL = "mongodb+srv://sthamilan0000_db_user:Milanstha10@backend.tehruof.
 const connectDB = async () => {
     try {
         await mongoose.connect(DB_URL);
+        mongoose.set("strictQuery", false);
+        
         console.log("Connected to the database");
     } catch (error) {
         console.log("Error connecting to the database", error);
